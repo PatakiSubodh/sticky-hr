43f0df2b5ba63e57ee8976757341496335ba3599
Subodh Pataki <135350282+PatakiSubodh@users.noreply.github.com>
2025-02-25 17:53:19 +0530

    restarted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
+import {Button} from './components/ui/button.jsx'
+import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './components/ui/card.jsx'
 
 function App() {
+  
+
   return (
     <>
-      <Navbar />
-      <Footer />
+      <Button>heLLO</Button>
+      <div className='flex items-center justify-center'>
+      <Card className="w-80">
+        <CardHeader>
+          <CardTitle>Hello</CardTitle>
+        </CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nesciunt voluptatem, omnis, sequi dolorum, saepe iusto optio illum voluptatibus neque quia animi sit tempore. Reiciendis ullam est culpa molestias aspernatur obcaecati, neque illo libero quam vel incidunt reprehenderit at voluptatibus.
+        </CardContent>
+        <CardFooter>End</CardFooter>
+      </Card>
+      </div>
+      
     </>
   )
 }
