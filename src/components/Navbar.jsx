25647c325bbb19900b96a9596adbbbe1661911a0
Subodh Pataki <135350282+PatakiSubodh@users.noreply.github.com>
2025-02-25 16:31:47 +0530

    added shadcn ui and tailwindCSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-
+import { Button } from "@/components/ui/button"
 
 export default function Navbar() {
     return (
         <>
             <h1>navbar</h1>
+            <Button>Click me</Button>
         </>
     );
 }
