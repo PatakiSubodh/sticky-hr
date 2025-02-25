248162f12ab1dca313d9a08c85f22ca66ef52ce3
Subodh Pataki <135350282+PatakiSubodh@users.noreply.github.com>
2025-02-25 20:57:55 +0530

    added logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
   return (
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import {Button} from '../components/ui/button.jsx'
+import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from '../components/ui/card.jsx'
+import logo from '../assets/images/image.png'
+
+function Navbar() {
+
+    return (
+        <>
+            <Button>heLLO</Button>
+            <div className='flex items-center justify-center'>
+            <Card className="w-80">
+                <CardHeader>
+                <CardTitle>Hello</CardTitle>
+                </CardHeader>
+                <CardContent>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nesciunt voluptatem, omnis, sequi dolorum, saepe iusto optio illum voluptatibus neque quia animi sit tempore. Reiciendis ullam est culpa molestias aspernatur obcaecati, neque illo libero quam vel incidunt reprehenderit at voluptatibus.
+                </CardContent>
+                <CardFooter>End</CardFooter>
+            </Card>
+            </div>
+            <img src={logo} alt="logo" />
+        </>
+    )
+}
+
+export default Navbar
